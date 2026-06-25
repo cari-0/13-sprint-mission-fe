795b0da0879b2e306ee6f726fcd97fcc64cc9330
cari <aa34500388@gmail.com>
2026-06-25 10:41:08 +0900

    시작

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,13 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-      <Link>회원가입 가는곳</Link>
-      <Link>상품 리스트 페이지 가는곳</Link>
-    </>
+    <div>
+      <li>
+        <Link href={"/l"}>회원가입 가는곳</Link>
+      </li>
+      <li>
+        <Link href={"/p"}>상품 리스트 페이지 가는곳</Link>
+      </li>
+    </div>
   );
 }
